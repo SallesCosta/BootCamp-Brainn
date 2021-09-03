cb457cf53884210c17d37754919e511b3b87d481
SallesCosta <newcapital.in@gmail.com>
2021-09-03 21:00:11 +0200

    #02 - 02 - Ok

--- 01/src/app.js
+++ 01/src/app.js
@@ -2,17 +2,26 @@ import Header from './components/header'
 import Menu from './components/menu'
 import Content from './components/content'
 import Footer from './components/footer'
+import Lista from './components/content'
+import { useState } from 'react'
 
 
 function App() {
+
+
+
     return (<>
         <Header></Header>
          <Menu></Menu>
         <Content></Content>
         <Footer></Footer> 
-
     </>)
 }
 
 
 export default App
+
+//uma observação. quando bolei meu layout.. eu pus o sidebar dentro do content...
+//entao isso muda minhas configurações.. mas estou preservando a intenção do exercício
+//o componente Sidebar e o array de artigos estao no componetne Content. E a lista é passada
+//como props para o Sidebar 
--- 01/src/components/content.js
+++ 01/src/components/content.js
@@ -1,43 +1,48 @@
 import Sidebar from './sidebar'
 import * as aux from './aux'
+import { useState } from 'react'
 
-function Content() {
+export default function Content() {
 
-const Lista = [
-    {
-        id: '01',
-        title: 'Primeiro Artigo',
-        content: 'conteúdo do pimeiro artigo'
-    },
-    {
-        id: '02',
-        title: 'Segundo Artigo',
-        content: 'conteúdo do segundo artigo'
-    },
-    {
-        id: '03',
-        title: 'Terceiro Artigo',
-        content: 'conteúdo do terceiro artigo'
-    },
-    {
-        id: '04',
-        title: 'Quarto Artigo',
-        content: 'conteúdo do quarto artigo'
-    },
-    {
-        id: '05',
-        title: 'Quinto Artigo',
-        content: 'conteúdo do quinto artigo'
-    }
-]
+    const Lista = [
+        {
+            id: '01',
+            title: 'Primeiro Artigo',
+            content: 'conteúdo do pimeiro artigo'
+        },
+        {
+            id: '02',
+            title: 'Segundo Artigo',
+            content: 'conteúdo do segundo artigo'
+        },
+        {
+            id: '03',
+            title: 'Terceiro Artigo',
+            content: 'conteúdo do terceiro artigo'
+        },
+        {
+            id: '04',
+            title: 'Quarto Artigo',
+            content: 'conteúdo do quarto artigo'
+        },
+        {
+            id: '05',
+            title: 'Quinto Artigo',
+            content: 'conteúdo do quinto artigo'
+        }
+    ]
+
+    const [title, setTitle] = useState(Lista[0].title)
+    const [content, setContent] = useState(Lista[0].content)
 
     return <>
-        <Sidebar lista={Lista}/>
+        <Sidebar lista={Lista} />
         <section className='content'>
             <aux.Github_e />
             <aux.Github_b />
         </section>
     </>
-}
 
-export default Content
+
+
+}
