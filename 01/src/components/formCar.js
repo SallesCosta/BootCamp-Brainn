25d5b463baa9be852e32bfb879c0bdf0fc1ba23c
SallesCosta <newcapital.in@gmail.com>
2021-09-04 11:11:00 +0200

    #3 - FormCar

--- 01/src/components/formCar.js
+++ 01/src/components/formCar.js
@@ -2,5 +2,32 @@ import { H2 } from "./titulos";
 
 
 export default function FormCar () {
-    return <H2 text='testando'></H2>
+    return <> 
+    {/* <H2 text='testando'></H2> */}
+    <form>
+        <div>
+            <label>Imagem</label>
+            <input/>
+        </div>
+        <div>
+            <label>Marca Modelo</label>
+            <input/>
+        </div>
+        <div>
+            <label>Ano</label>
+            <input/>
+        </div>
+        <div>
+            <label>Placa</label>
+            <input/>
+        </div>
+        <div>
+            <label>Cor</label>
+            <input/>
+        </div>
+        <button className='primary'>Cadastrar</button>            
+    </form>
+
+
+    </>
 }
