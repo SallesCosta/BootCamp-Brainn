f27e9c9bb74ca18b2880cf426564744a6d990bd2
SallesCosta <newcapital.in@gmail.com>
2021-09-04 11:42:33 +0200

    #3 - FormCar

--- 01/src/components/formCar.js
+++ 01/src/components/formCar.js
@@ -1,29 +1,49 @@
 import { H2 } from "./titulos";
-
+import { useState } from "react";
 
 export default function FormCar () {
+    const [imgValue, setImgValue] = useState('') 
+    console.log('imgValue: ', imgValue)
+    
+    const [modelValue, setModelValue] = useState('') 
+    console.log('modelValue: ', modelValue)
+    
+    const [anoValue, setAnoValue] = useState('') 
+    console.log('anoValue: ', anoValue)
+    
+    const [placaValue, setPlacaValue] = useState('') 
+    console.log('placaValue: ', placaValue)
+    
+    const [corValue, setCorvalue] = useState('') 
+    console.log('corValue: ', corValue)
+
+    function handleChange (e) {
+
+    }
+    
+
     return <div className='d-flex card body'> 
     {/* <H2 text='testando'></H2> */}
     <form >
         <div>
             <label>Imagem</label>
-            <input/>
+            <input value={imgValue} onChange={handleChange}/>
         </div>
         <div>
             <label>Marca Modelo</label>
-            <input/>
+            <input value={modelValue} onChange={handleChange}/>
         </div>
         <div>
             <label>Ano</label>
-            <input/>
+            <input value={anoValue} onChange={handleChange}/>
         </div>
         <div>
             <label>Placa</label>
-            <input/>
+            <input value={placaValue} onChange={handleChange}/>
         </div>
         <div>
             <label>Cor</label>
-            <input/>
+            <input value={corValue} onChange={handleChange}/>
         </div>
         <button className='primary'>Cadastrar</button>            
     </form>
